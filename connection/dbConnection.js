145e6526fcdc226642c7457b572e57bd78867d73
unknown <shehabeldeenkhamis@gmail.com>
2022-02-17 22:19:33 +0200

    last commit isa

--- connection/dbConnection.js
+++ connection/dbConnection.js
@@ -1,9 +1,9 @@
 const db = require('mysql2')
 const pool=db.createPool({
-    host : 'localhost',
-    user :'root',
-    database : 'the_sparks_foundation',
-    password : 'root'
+    host : 'eu-cdbr-west-02.cleardb.net',
+    user :'b69ce6a6bce2cc',   
+    database : 'heroku_efe922b1038eb7f',
+    password : '81ec1881'
 });
 
 module.exports=pool.promise();
